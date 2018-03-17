ccf0b9370a4fe731dbf3e87dc340a035dd36e9be
Fabian Todt <mail@fabiantodt.at>
2018-03-17 11:32:47 +0100

    refactored index to not use constructor and configured eslint & fixed eslint errors

--- src/index.js
+++ src/index.js
@@ -1,235 +1,220 @@
-import { default as cookieStorage } from './storages/cookie';
-import { default as localStorage } from './storages/localStorage';
-import { default as dataStorage } from './storages/data';
-import { default as gtmPlugin } from './plugins/gtm';
-import { default as linkPlugin } from './plugins/link'; 
+import * as cookieStorage from './storages/cookie';
+import * as localStorage from './storages/localStorage';
+import * as dataStorage from './storages/data';
+import * as gtmPlugin from './plugins/gtm';
+import * as linkPlugin from './plugins/link';
 
 // storage needs to have get & set methods
 // service needs to have mode (optIn|optOut)
 
-function optInOut(userOptions) {
+const OptInOut = (userOptions) => {
+  const self = {};
 
-  function OptInOut(userOptions) {
-    const self = this;
-    const defaultOptions = {
-      storages: {
-        'cookie': cookieStorage(),
-        'localStorage': localStorage(),
-        'dataStorage': dataStorage()
-      },
-      services: {
+  const defaultOptions = {
+    storages: {
+      cookie: cookieStorage(),
+      localStorage: localStorage(),
+      dataStorage: dataStorage(),
+    },
+    services: {
 
-      },
-      plugins: [],
-    };
-
-    //PRIVATE PROPERTIES
-
-    let options;
-    let storages;
-    let services;
-    let events;
-
-    //PRIVATE METHODS
-
-    let setValueInStorages = (serviceKey, key, value, storageKey = false, update = true) => {
-      if (!services[serviceKey]) throw new Error('service ' + serviceKey + ' is not configured for OptInOut');
-
-      if (storageKey && storages[storageKey]) {
-        storages[storageKey].set(serviceKey, key, value);
-      } else {
-        for (let storagesKey in storages) {
-          if (!storages.hasOwnProperty(storagesKey)) continue;
-          storages[storagesKey].set(serviceKey, key, value, update);
-        }
-      }
-    };
-
-    let triggerEvent = (event,data) => {
-      let handlers = events[event]; 
-      if(!handlers || handlers.length < 1) {
-        return; 
-      }
-
-      handlers.forEach((handler) => {
-        handler.call(self, [data,event]); 
-      })
-    };
-
-    //PUBLIC PROPERTIES 
-
-    //PUBLIC METHODS 
-    self.optIn = (serviceKey, storageKey = false) => {
-      let date = new Date();
-      setValueInStorages(serviceKey, 'optedIn', date , storageKey);
-      triggerEvent('optIn', { service: serviceKey, storage: storageKey, date: date });
-    };
-
-    self.optOut = (serviceKey, storageKey = false) => {
-      setValueInStorages(serviceKey, 'optedOut', new Date(), storageKey);
-      triggerEvent('optOut', { service: serviceKey, storage: storageKey, date: date });
-    };
-
-    self.getOption = (optionKey) => {
-      return options[optionKey];
-    };
-
-    self.getStorage = (storageKey) => {
-      return storages[storageKey];
-    };
-
-    self.getService = (serviceKey) => {
-      return services[serviceKey];
-    };
-
-    self.getStorages = () => storages; 
-
-    self.getServices = () => services;
-
-    self.getOptions = () => options;
-
-    self.on = (event,callback) => {
-      let handlers = events[event] || []; 
-      handlers.push(callback); 
-      events[event] = handlers; 
-    };
-
-    self.isAllowed = (serviceKey, storageKey) => {
-      let service;
-      let checkStorages;
-      if (serviceKey && services[serviceKey]) {
-        service = services[serviceKey];
-        if (storageKey && storages[storageKey]) {
-          let storagesCopy = Object.assign({},storages); 
-          delete storagesCopy[storageKey];
-          checkStorages = [storageKey, ...Object.keys(storagesCopy)];         
-        } else {
-          checkStorages = Object.keys(storages);
-        }
-      }
-
-      let allowed = null; //default
-
-      for (let currentStorageKey of checkStorages) {
-        let storage = storages[currentStorageKey];
-        let value = storage.get(serviceKey);
-
-        if (typeof value === 'undefined' || value === undefined || value === null) continue;
-
-        if (service.mode == 'optIn') {
-          if (value.optedIn == false) {
-            allowed = false;
-          } else if (value.optedIn && !value.optedOut) {
-            allowed = true;
-          } else if (Date.parse(value.optedOut) < Date.parse(value.optedIn)) {
-            allowed = true;
-          } else if (Date.parse(value.optedOut) > Date.parse(value.optedIn)) {
-            allowed = false;
-          }
-        } else if (service.mode == 'optOut') {
-          if (value.optedIn == undefined || value.optedIn == false) {
-            allowed = !value.optedOut;
-          } else if (Date.parse(value.optedIn) > Date.parse(value.optedOut)) {
-            allowed = true;
-          } else if (Date.parse(value.optedIn) < Date.parse(value.optedOut)) {
-            allowed = false;
-          }
-        }
-
-        if (allowed !== null) break;
-      }
-
-      if (allowed === null) { //need to use default value
-        allowed = service.default ? service.default : (service.mode == 'optIn' ? false : true);
-      }
-
-      return allowed;
-    };
-
-    //INIT OBJECT
-
-    //PROPERTIES
-    //options
-    options = Object.assign({}, defaultOptions, userOptions);
-
-    let initStorages = () => {
-      storages = {};
-      for (let storagesKey in options.storages) {
-        if (!options.storages.hasOwnProperty(storagesKey)) continue;
-        let storage = options.storages[storagesKey];
-        if (typeof storage.get === 'function' && typeof storage.set === 'function') {
-          storages[storagesKey] = options.storages[storagesKey];
-        }
-      }
-      delete options.storages;
-    };
-    initStorages();
-
-    let initServices = () => {
-      services = {};
-      for (let serviceKey in options.services) {
-        if (!options.services.hasOwnProperty(serviceKey)) continue;
-        let service = options.services[serviceKey];
-        if (typeof service.mode !== 'undefined') {
-          services[serviceKey] = options.services[serviceKey];
-        }
-      }
-      delete options.services;
-    };
-    initServices();
-
-    let initPlugins = () => {
-      events = {}; 
-      plugins.forEach((plugin) => {
-        if(typeof plugin.init !== 'function') {
-          plugin.init(self); 
-        }
-      });
-    }
-    initPlugins(); 
+    },
+    plugins: [],
   };
 
-  var newObject = new OptInOut(userOptions); 
+  // PRIVATE PROPERTIES
 
-  if(optInOut.instance === undefined) {
-    optInOut.instance = newObject; 
-  }
+  let options;
+  let storages;
+  let services;
+  let events;
 
-  return newObject; 
+  // PRIVATE METHODS
+
+  const setValueInStorages = (serviceKey, key, value, storageKey = false, update = true) => {
+    if (!services[serviceKey]) throw new Error(`service ${serviceKey} is not configured for OptInOut`);
+
+    if (storageKey && storages[storageKey]) {
+      storages[storageKey].set(serviceKey, key, value);
+    } else {
+      Object.keys(storages).forEach((storagesKey) => {
+        storages[storagesKey].set(serviceKey, key, value, update);
+      });
+    }
+  };
+
+  const triggerEvent = (event, data) => {
+    const handlers = events[event];
+    if (!handlers || handlers.length < 1) {
+      return;
+    }
+
+    handlers.forEach((handler) => {
+      handler.call(self, [data, event]);
+    });
+  };
+
+  // PUBLIC PROPERTIES
+
+  // PUBLIC METHODS
+  self.optIn = (serviceKey, storageKey = false) => {
+    const date = new Date();
+    setValueInStorages(serviceKey, 'optedIn', date, storageKey);
+    triggerEvent('optIn', { service: serviceKey, storage: storageKey, date });
+  };
+
+  self.optOut = (serviceKey, storageKey = false) => {
+    const date = new Date();
+    setValueInStorages(serviceKey, 'optedOut', date, storageKey);
+    triggerEvent('optOut', { service: serviceKey, storage: storageKey, date });
+  };
+
+  self.getOption = optionKey => options[optionKey];
+
+  self.getStorage = storageKey => storages[storageKey];
+
+  self.getService = serviceKey => services[serviceKey];
+
+  self.getStorages = () => storages;
+
+  self.getServices = () => services;
+
+  self.getOptions = () => options;
+
+  self.on = (event, callback) => {
+    const handlers = events[event] || [];
+    handlers.push(callback);
+    events[event] = handlers;
+  };
+
+  self.isAllowed = (serviceKey, storageKey) => {
+    let service;
+    let checkStorages;
+    if (serviceKey && services[serviceKey]) {
+      service = services[serviceKey];
+      if (storageKey && storages[storageKey]) {
+        const storagesCopy = Object.assign({}, storages);
+        delete storagesCopy[storageKey];
+        checkStorages = [storageKey, ...Object.keys(storagesCopy)];
+      } else {
+        checkStorages = Object.keys(storages);
+      }
+    }
+
+    let allowed = null; // default
+
+    Object.keys(checkStorages).every((currentStorageKey) => {
+      const storage = storages[currentStorageKey];
+      const value = storage.get(serviceKey);
+
+      if (typeof value === 'undefined' || value === undefined || value === null) return true; // continue
+
+      if (service.mode === 'optIn') {
+        if (value.optedIn === false) {
+          allowed = false;
+        } else if (value.optedIn && !value.optedOut) {
+          allowed = true;
+        } else if (Date.parse(value.optedOut) < Date.parse(value.optedIn)) {
+          allowed = true;
+        } else if (Date.parse(value.optedOut) > Date.parse(value.optedIn)) {
+          allowed = false;
+        }
+      } else if (service.mode === 'optOut') {
+        if (value.optedIn === undefined || !value.optedIn) {
+          allowed = !value.optedOut;
+        } else if (Date.parse(value.optedIn) > Date.parse(value.optedOut)) {
+          allowed = true;
+        } else if (Date.parse(value.optedIn) < Date.parse(value.optedOut)) {
+          allowed = false;
+        }
+      }
+
+      if (allowed !== null) return false; // break
+      return true;
+    });
+
+    if (allowed === null) { // need to use default value
+      allowed = service.default ? service.default : (service.mode !== 'optIn');
+    }
+
+    return allowed;
+  };
+
+  // INIT OBJECT
+
+  // PROPERTIES
+  // options
+  options = Object.assign({}, defaultOptions, userOptions);
+
+  // init storages
+  storages = {};
+  Object.keys(options.storages).forEach((storagesKey) => {
+    const storage = options.storages[storagesKey];
+    if (typeof storage.get === 'function' && typeof storage.set === 'function') {
+      storages[storagesKey] = options.storages[storagesKey];
+    }
+    return true;
+  });
+  delete options.storages;
+
+  // init services
+  services = {};
+  Object.keys(options.services).forEach((serviceKey) => {
+    const service = options.services[serviceKey];
+    if (typeof service.mode !== 'undefined') {
+      services[serviceKey] = options.services[serviceKey];
+    }
+  });
+  delete options.services;
+
+  // init plugins
+  events = {};
+  options.plugins.forEach((plugin) => {
+    if (typeof plugin.init !== 'function') {
+      plugin.init(self);
+    }
+  });
+
+
+  OptInOut.instance = self;
+  return self;
 };
 
-optInOut.isAllowed = (service,storage) => {
-  if(optInOut.instance !== undefined) {
-    return optInOut.instance.isAllowed(service,storage); 
+// set global accessable methods
+OptInOut.isAllowed = (service, storage) => {
+  if (OptInOut.instance !== undefined) {
+    return OptInOut.instance.isAllowed(service, storage);
   }
   return null;
 };
 
-optInOut.optIn = (service,storage) => {
-  if(optInOut.instance !== undefined) {
-    return optInOut.instance.optIn(service,storage); 
+OptInOut.optIn = (service, storage) => {
+  if (OptInOut.instance !== undefined) {
+    return OptInOut.instance.optIn(service, storage);
   }
-  return null; 
+  return null;
 };
 
-optInOut.optOut = (service,storage) => {
-  if(optInOut.instance !== undefined) {
-    return optInOut.instance.optOut(service,storage); 
+OptInOut.optOut = (service, storage) => {
+  if (OptInOut.instance !== undefined) {
+    return OptInOut.instance.optOut(service, storage);
   }
-  return null; 
+  return null;
 };
 
+// set global accessable/namespaced storageadapters
 const storageAdapters = {
-  'cookieStorage': cookieStorage,
-  'localStorage': localStorage,
-  'dataStorage': dataStorage
+  cookieStorage,
+  localStorage,
+  dataStorage,
 };
-optInOut.storageAdapters = storageAdapters;
+OptInOut.storageAdapters = storageAdapters;
 
+// set global accessable/namespaced plugins
 const plugins = {
-  'gtmPlugin': gtmPlugin,
-  'linkPlugin': linkPlugin
-}; 
-optInOut.plugins = gtmPlugin; 
+  gtmPlugin,
+  linkPlugin,
+};
+OptInOut.plugins = plugins;
 
-export default optInOut;
+export default OptInOut;
--- src/storages/cookie.js
+++ src/storages/cookie.js
@@ -1,27 +1,3 @@
-/*\
-|*|
-|*|	:: cookies.js ::
-|*|
-|*|	A complete cookies reader/writer framework with full unicode support.
-|*|
-|*|	Revision #3 - July 13th, 2017
-|*|
-|*|	https://developer.mozilla.org/en-US/docs/Web/API/document.cookie
-|*|	https://developer.mozilla.org/User:fusionchess
-|*|	https://github.com/madmurphy/cookies.js
-|*|
-|*|	This framework is released under the GNU Public License, version 3 or later.
-|*|	http://www.gnu.org/licenses/gpl-3.0-standalone.html
-|*|
-|*|	Syntaxes:
-|*|
-|*|	* docCookies.setItem(name, value[, end[, path[, domain[, secure]]]])
-|*|	* docCookies.getItem(name)
-|*|	* docCookies.removeItem(name[, path[, domain]])
-|*|	* docCookies.hasItem(name)
-|*|
-\*/
-
 const Storage = (userOptions) => {
   const defaultOptions = {
     namespace: 'optInOut',
@@ -31,112 +7,91 @@ const Storage = (userOptions) => {
     secure: false,
   };
 
-  let options = Object.assign({}, defaultOptions, userOptions);
+  const options = Object.assign({}, defaultOptions, userOptions);
 
 
-  let getExpirationString = (date) => {
-    date = date ? date : options.expiration;
+  const getExpirationString = (date) => {
+    const expirationDate = date || options.expiration;
 
-    let expires; 
-    switch (date.constructor) {
+    let expires;
+    switch (expirationDate.constructor) {
       case Number:
-        expires = date === Infinity ? "; expires=Fri, 31 Dec 9999 23:59:59 GMT" : "; max-age=" + date;
-        /*
-        Note: Despite officially defined in RFC 6265, the use of `max-age` is not compatible with any
-        version of Internet Explorer, Edge and some mobile browsers. Therefore passing a number to
-        the end parameter might not work as expected. A possible solution might be to convert the the
-        relative time to an absolute time. For instance, replacing the previous line with:
-        */
-        /*
-        sExpires = vEnd === Infinity ? "; expires=Fri, 31 Dec 9999 23:59:59 GMT" : "; expires=" + (new Date(vEnd * 1e3 + Date.now())).toUTCString();
-        */
+        expires = date === Infinity ? '; expires=Fri, 31 Dec 9999 23:59:59 GMT' : `; max-age=${expirationDate}`;
         break;
       case String:
-        expires = "; expires=" + date;
+        expires = `; expires=${expirationDate}`;
         break;
       case Date:
-        expires = "; expires=" + date.toUTCString();
+        expires = `; expires=${expirationDate.toUTCString()}`;
+        break;
+      default:
+        expires = '';
         break;
     }
 
     return expires;
-  }
+  };
 
-  let getKey = (key) => {
-    return options.namespace + '.' + key; 
-  }
+  const getKey = key => `${options.namespace}.${key}`;
 
-  let getItem = function (service,key) {
+  const getValue = (key) => {
+    if (!key || /^(?:expires|max-age|path|domain|secure)$/i.test(key)) { return false; }
+    return decodeURIComponent(document.cookie.replace(new RegExp(`(?:(?:^|.*;)\\s*${encodeURIComponent(getKey(key)).replace(/[-.+*]/g, '\\$&')}\\s*\\=\\s*([^;]*).*$)|^.*$`), '$1'));
+  };
+
+  const getItem = (service, key) => {
     if (!service) { return null; }
 
-    let value = getValue(service) || null;
+    const value = getValue(service) || null;
 
-    let valueJson = JSON.parse(value); 
-    if(key) {
-      return valueJson[key] || null; 
-    } else {
-      return valueJson || null; 
+    const valueJson = JSON.parse(value);
+    if (key) {
+      return valueJson[key] || null;
     }
+    return valueJson || null;
   };
 
-  let writeValue = (k,v,expires) => {
-    document.cookie = encodeURIComponent(k) + "=" + encodeURIComponent(JSON.stringify(v)) + getExpirationString(expires) + (options.domain ? "; domain=" + options.domain : "") + (options.Path ? "; path=" + options.Path : "") + (options.secure ? "; secure" : "");
+  const writeValue = (key, value, expires) => {
+    document.cookie = `${encodeURIComponent(key)}=${encodeURIComponent(JSON.stringify(value))}${getExpirationString(expires)}${options.domain ? `; domain=${options.domain}` : ''}${options.Path ? `; path=${options.Path}` : ''}${options.secure ? '; secure' : ''}`;
   };
 
-  let getValue = (key) => {
-    if (!key || /^(?:expires|max\-age|path|domain|secure)$/i.test(key)) { return false; }
-    return decodeURIComponent(document.cookie.replace(new RegExp("(?:(?:^|.*;)\\s*" + encodeURIComponent(getKey(key)).replace(/[\-\.\+\*]/g, "\\$&") + "\\s*\\=\\s*([^;]*).*$)|^.*$"), "$1"));
-  };
+  const setItem = (service, key, value, update) => {
+    if (!service || /^(?:expires|max-age|path|domain|secure)$/i.test(service)) { return false; }
 
-  let setItem = function (service,key,value,update) {
-    if (!service || /^(?:expires|max\-age|path|domain|secure)$/i.test(service)) { return false; }
-    
     let saveValue;
 
-    if(update) {
-      let newData = { [key]: value };
-      let currentData = getItem(service); 
-      saveValue = Object.assign({}, currentData, newData); 
+    if (update) {
+      const newData = { [key]: value };
+      const currentData = getItem(service);
+      saveValue = Object.assign({}, currentData, newData);
     } else {
-      saveValue = { [key]: value }; 
+      saveValue = { [key]: value };
     }
 
-    writeValue(getKey(service),saveValue);
-    
+    writeValue(getKey(service), saveValue);
+
     return true;
   };
 
-  let removeItem = function (service,key) {
+  const hasItem = service => Boolean(getValue(service));
 
-    if(!service || !hasItem(service)) { return false; }
+  const removeItem = (service, key) => {
+    if (!service || !hasItem(service)) { return false; }
 
-    if(key) {
-      let currentValue = getItem(service,key); 
-      delete currentValue[key]; 
-      return setItem(service,key,currentValue,false); //force overwrite
-    } else {
-      writeValue(getKey(service),'', new Date('01 Jan 1970')); 
-      return true; 
+    if (key) {
+      const currentValue = getItem(service, key);
+      delete currentValue[key];
+      return setItem(service, key, currentValue, false); // force overwrite
     }
-  };
-
-  let hasItem = function (service) {
-    return Boolean(getValue(service));
+    writeValue(getKey(service), '', new Date('01 Jan 1970'));
+    return true;
   };
 
   return {
-    get: (service,key = false) => {
-      return getItem(service,key);
-    },
-    set: (service,key,value,update = true) => {
-      return setItem(service,key,value,update);
-    },
-    delete: (service,key = false) => {
-      return removeItem(service,key);
-    }
+    get: (service, key = false) => getItem(service, key),
+    set: (service, key, value, update = true) => setItem(service, key, value, update),
+    delete: (service, key = false) => removeItem(service, key),
   };
-
-}
-
+};
 
 export default Storage;
