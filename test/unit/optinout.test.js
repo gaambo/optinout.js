bda9f3f67fb4a24ef23d55cc5d0bb19dcd0e1adc
Fabian Todt <fabian@fabiantodt.at>
2019-08-27 14:53:33 +0200

    added tests and fixed bugs with isSet

--- test/unit/optinout.test.js
+++ test/unit/optinout.test.js
@@ -1,6 +1,20 @@
 import OptInOut from '../../src/main';
 import DataStorage from '../../src/storages/data';
 
+test("isSet to be false by default", () => {
+  let dataStorage = DataStorage({});
+  let obj = OptInOut({
+    storages: {
+      data: dataStorage,
+    },
+    services: {
+      analytics: { mode: 'optOut' }
+    }
+  });
+
+  expect(obj.isSet('analytics')).toBe(false);
+});
+
 test("Optout of Service with optOut Mode", () => {
   let dataStorage = DataStorage({});
   let obj = OptInOut({
@@ -13,10 +27,12 @@ test("Optout of Service with optOut Mode", () => {
   });
 
   expect(obj.isAllowed('analytics')).toBe(true);
+  expect(obj.isSet('analytics')).toBe(false);
 
   obj.optOut('analytics');
 
   expect(obj.isAllowed('analytics')).toBe(false);
+  expect(obj.isSet('analytics')).toBe(true);
 });
 
 test("Optin to Service with optIn mode", () => {
@@ -31,10 +47,33 @@ test("Optin to Service with optIn mode", () => {
   });
 
   expect(obj.isAllowed('facebook')).toBe(false);
+  expect(obj.isSet('facebook')).toBe(false);
 
   obj.optIn('facebook');
 
   expect(obj.isAllowed('facebook')).toBe(true);
+  expect(obj.isSet('facebook')).toBe(true);
+
+});
+
+test("Optout to services with optIn mode", () => {
+  let dataStorage = DataStorage({});
+  let obj = OptInOut({
+    storages: {
+      data: dataStorage,
+    },
+    services: {
+      analytics: { mode: 'optIn' }
+    }
+  });
+
+  expect(obj.isAllowed('analytics')).toBe(false);
+  expect(obj.isSet('analytics')).toBe(false);
+
+  obj.optOut('analytics');
+
+  expect(obj.isAllowed('analytics')).toBe(false);
+  expect(obj.isSet('analytics')).toBe(true);
 
 });
 
@@ -66,10 +105,12 @@ test("Optin to Service with optIn mode with specified storage", () => {
   });
 
   expect(obj.isAllowed('facebook', 'data')).toBe(false);
+  expect(obj.isSet('facebook', 'data')).toBe(false);
 
   obj.optIn('facebook', 'data');
 
   expect(obj.isAllowed('facebook', 'data')).toBe(true);
+  expect(obj.isSet('facebook', 'data')).toBe(true);
 
 });
 
@@ -85,10 +126,12 @@ test("Optout of Service with optOut Mode with specified storage", () => {
   });
 
   expect(obj.isAllowed('analytics', 'data')).toBe(true);
+  expect(obj.isSet('analytics')).toBe(false);
 
   obj.optOut('analytics', 'data');
 
   expect(obj.isAllowed('analytics', 'data')).toBe(false);
+  expect(obj.isSet('analytics')).toBe(true);
 });
 
 test("Optin after opting out - with optIn service - checks date", () => { //TODO should check <= oder so? weil selbe zeit genau 
@@ -105,12 +148,14 @@ test("Optin after opting out - with optIn service - checks date", () => { //TODO
   obj.optOut('facebook');
 
   expect(obj.isAllowed('facebook')).toBe(false);
+  expect(obj.isSet('facebook')).toBe(true);
 
   //wait so time of optin and optout is not the same
   setTimeout(() => {
     obj.optIn('facebook');
 
     expect(obj.isAllowed('facebook')).toBe(true);
+    expect(obj.isSet('facebook')).toBe(true);
   }, 1000);
 
 
@@ -130,10 +175,15 @@ test("Optout after opting in - with optIn service - checks date", () => {
   obj.optIn('facebook');
 
   expect(obj.isAllowed('facebook')).toBe(true);
+  expect(obj.isSet('facebook')).toBe(true);
 
-  obj.optOut('facebook');
+  //wait so date of optin and optout is not the same
+  setTimeout(() => {
+    obj.optOut('facebook');
 
-  expect(obj.isAllowed('facebook')).toBe(false);
+    expect(obj.isAllowed('facebook')).toBe(false);
+    expect(obj.isSet('facebook')).toBe(true);
+  }, 1000);
 });
 
 test("Optin after opting out - with optOut service - checks date", () => {
@@ -150,10 +200,15 @@ test("Optin after opting out - with optOut service - checks date", () => {
   obj.optOut('analytics');
 
   expect(obj.isAllowed('analytics')).toBe(false);
+  expect(obj.isSet('analytics')).toBe(true);
 
-  obj.optIn('analytics');
+  //wait so date of optin and optout is not the same
+  setTimeout(() => {
+    obj.optIn('analytics');
 
-  expect(obj.isAllowed('analytics')).toBe(true);
+    expect(obj.isAllowed('analytics')).toBe(true);
+    expect(obj.isSet('analytics')).toBe(true);
+  }, 1000);
 });
 
 test("Optout after opting in - with optOut service - checks date", () => {
@@ -168,16 +223,19 @@ test("Optout after opting in - with optOut service - checks date", () => {
   });
 
   expect(obj.isAllowed('analytics')).toBe(true);
+  expect(obj.isSet('analytics')).toBe(false);
 
   obj.optIn('analytics');
 
   expect(obj.isAllowed('analytics')).toBe(true);
+  expect(obj.isSet('analytics')).toBe(true);
 
   //wait so date of optin and optout is not the same
   setTimeout(() => {
     obj.optOut('analytics');
 
     expect(obj.isAllowed('analytics')).toBe(false);
+    expect(obj.isSet('analytics')).toBe(true);
   }, 1000);
 
 
@@ -197,6 +255,7 @@ test("Optedin set to false explicitly", () => {
   });
 
   expect(obj.isAllowed('facebook')).toBeFalsy();
+  expect(obj.isSet('facebook')).toBe(true);
 });
 
 test("Set doNotTrack but use default (no browser)", () => {
@@ -212,6 +271,7 @@ test("Set doNotTrack but use default (no browser)", () => {
   });
 
   expect(obj.isAllowed('analytics')).toBe(true);
+  expect(obj.isSet('analytics')).toBe(false);
 });
 
 test("Check Status Message - OptIn and OptOut", () => {
@@ -242,6 +302,5 @@ test("Check Status Message - OptIn and OptOut", () => {
     obj.optOut('facebook');
     expect(obj.getStatus('facebook')).toEqual('opted out');
   }, 1000);
-
 
 });
